0251b36be15a714d68196bf93617425fe73a4bfd
Darren Frey <darren.frey@gmail.com>
2014-09-08 21:56:08 +0200

    test with spyder-generated script

---
+++ minipygame.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pygame
+pygame.init()
+w=pygame.display.set_mode([300,300])
+w.fill([128,37,213])
+pygame.display.flip()
+pygame.time.wait(3000)
+pygame.quit()
+
